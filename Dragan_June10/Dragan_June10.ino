6e423a90700185f9e437a481d9ed39d36cab8dfa
DraganKov <dragsterd@yahoo.ca>
2017-10-11 16:19:14 -0400

    Inital
    Use Arduino IDE to view

---
+++ Dragan_June10/Dragan_June10.ino
@@ -0,0 +1,130 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <RobotOpen.h>
+
+/* I/O Setup */
+USBJoystick usb1('0');  // Assign the logitech USBJoystick object to bundle 0
+
+void setup(){
+  /* Initiate comms */
+  RobotOpen.begin();
+}
+
+/* This is your primary robot loop - all of your code
+ * should live here that allows the robot to operate
+ */
+void enabled() {
+  pinMode(SIDECAR_DIGITAL7, OUTPUT);
+  
+  //Compressor 
+  if(usb1.getIndex(BTN9)){
+    digitalWrite(SIDECAR_DIGITAL7, LOW); //LOW == ON
+  }
+  else{
+    digitalWrite(SIDECAR_DIGITAL7, HIGH); //HIGH == OFF
+  }
+
+  //Drive motors
+  RobotOpen.setPWM(SIDECAR_PWM1, usb1.makePWM(ANALOG_RIGHTY, INVERT));//Right Motor
+  RobotOpen.setPWM(SIDECAR_PWM2, usb1.makePWM(ANALOG_LEFTY, NORMAL));//Left Motor
+
+  //Shifter Servos
+  RobotOpen.setPWM(SIDECAR_PWM4, usb1.makePWM(RIGHT_ANALOG_BTN, INVERT));//Right Shifter
+  RobotOpen.setPWM(SIDECAR_PWM3, usb1.makePWM(RIGHT_ANALOG_BTN, INVERT));//Left Shifter
+
+  //X-axis  
+  if(usb1.getIndex(BTN3)){
+      RobotOpen.setPWM(SIDECAR_PWM9, 122);//x-axis left rotation
+  }
+  else if(usb1.getIndex(BTN1)){
+      RobotOpen.setPWM(SIDECAR_PWM9, 145);//x-axis right rot
+  }
+  else{
+    RobotOpen.setPWM(SIDECAR_PWM9, 130);
+  }
+  
+  //Y-AXIS
+  if(usb1.getIndex(BTN2)){
+    RobotOpen.setPWM(SIDECAR_PWM10, 140);
+  }
+  else if(usb1.getIndex(BTN4){
+    RobotOpen.setPWM(SIDECAR_PWM10, 120);
+  }
+  else{
+    RobotOpen.setPWM(SIDECAR_PWM10, 130);
+   
+    
+  //INTAKE ROLLER CODE
+  if(usb1.getIndex(BTN5)){
+    RobotOpen.setPWM(SIDECAR_PWM5, 40);  //INTAKE
+  }
+  else if(usb1.getIndex(BTN6)){
+    RobotOpen.setPWM(SIDECAR_PWM5, 210); //EXAUST
+  }
+  else{
+    RobotOpen.setPWM(SIDECAR_PWM5, 127); // REST
+  }
+  
+  //Shooter Motors
+  if(usb1.getIndex(BTN10)){
+    RobotOpen.setPWM(SIDECAR_PWM7, 75);
+  }
+   else{
+     RobotOpen.setPWM(SIDECAR_PWM7, 127);
+   }
+}
+
+
+/* This is called while the robot is disabled
+ * You must make sure to set all of your outputs
+ * to safe/disable values here*/
+void disabled(){
+  // PWMs are automatically disabled
+}
+
+
+/* This loop ALWAYS runs - only place code here that can run during a disabled state
+ * This is also a good spot to put driver station publish code
+ * You can use either publishAnalog, publishDigital, publishByte, publishShort, or publishLong
+ * Specify a bundle ID with a single character (a-z, A-Z, 0-9) - Just make sure not to use the same twice!
+ */
+void timedtasks() {
+  
+  RobotOpen.publishDigital(usb1.getIndex(BTN1), '1');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN2), '2');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN3), '3');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN4), '4');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN5), '5');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN6), '6');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN7), '7');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN8), '8');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN9), '9');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(BTN10), '10');   // Bundle 
+  
+  RobotOpen.publishAnalog(usb1.getIndex(ANALOG_RIGHTY), 'A');   // Bundle 
+  RobotOpen.publishAnalog(usb1.getIndex(ANALOG_LEFTY), 'B');   // Bundle 
+  
+  RobotOpen.publishDigital(usb1.getIndex(RIGHT), 'C');   // Bundle 
+  RobotOpen.publishDigital(usb1.getIndex(LEFT), 'D');   // Bundle 
+
+  
+
+
+}
+
+
+/* This is the main program loop that keeps comms operational
+ * There's no need to touch anything here!!!
+ */
+void loop() {
+  RobotOpen.pollDS();
+  if(RobotOpen.enabled())
+    enabled();
+  else
+    disabled();
+  timedtasks();
+  RobotOpen.outgoingDS();
+}
+
+
+
---
+++ TankDrive/TankDrive.ino
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <RobotOpen.h>
+#include <Serial.h>
+
+/* I/O Setup */
+USBJoystick usb1('0');  // Assign the logitech USBJoystick object to bundle 0
+
+void setup(){
+/* Initiate comms */
+RobotOpen.begin();
+}
+
+
+/* This is your primary robot loop - all of your code
+ * should live here that allows the robot to operate
+ */
+void enabled() {
+  //Shifter Variables
+  //int highGear = 200, lowGear = 50; //PWM Values for Servo's   
+
+  //Drive motors
+  RobotOpen.setPWM(SIDECAR_PWM1, usb1.makePWM(ANALOG_LEFTY, NORMAL));
+  RobotOpen.setPWM(SIDECAR_PWM2, usb1.makePWM(ANALOG_RIGHTY, INVERT));
+
+  //Current Shifter Setup
+  RobotOpen.setPWM(SIDECAR_PWM4,usb1.makePWM(BTN1,INVERT)); //THIS WORKS
+  RobotOpen.setPWM(SIDECAR_PWM3,usb1.makePWM(BTN1,INVERT)); //THIS WORKS
+
+  if(usb1.getIndex(BTN5)){
+    RobotOpen.setPWM(SIDECAR_PWM5, 20); 
+  }
+  else if(usb1.getIndex(BTN6)){
+    RobotOpen.setPWM(SIDECAR_PWM5, 230);
+  }
+  else
+    RobotOpen.setPWM(SIDECAR_PWM5, 127);
+}  
+
+
+/* This is called while the robot is disabled
+ * You must make sure to set all of your outputs
+ * to safe/disable values here
+ */
+void disabled() {
+// PWMs are automatically disabled
+}
+
+
+/* This loop ALWAYS runs - only place code here that can run during a disabled state
+ * This is also a good spot to put driver station publish code
+ * You can use either publishAnalog, publishDigital, publishByte, publishShort, or publishLong
+ * Specify a bundle ID with a single character (a-z, A-Z, 0-9) - Just make sure not to use the same twice!
+ */
+void timedtasks() {
+  RobotOpen.publishAnalog(ANALOG0, 'A');   // Bundle A
+  RobotOpen.publishAnalog(ANALOG1, 'B');   // Bundle B
+  RobotOpen.publishAnalog(ANALOG2, 'C');   // Bundle C
+  RobotOpen.publishAnalog(ANALOG3, 'D');   // Bundle D
+  RobotOpen.publishAnalog(ANALOG4, 'E');   // Bundle E
+  RobotOpen.publishAnalog(ANALOG5, 'F');   // Bundle 
+}
+
+
+/* This is the main program loop that keeps comms operational
+* There's no need to touch anything here!!!
+*/
+void loop() {
+  RobotOpen.pollDS();
+  if(RobotOpen.enabled())
+    enabled();
+  else
+    disabled();
+    timedtasks();
+  RobotOpen.outgoingDS();
+}
+
+
